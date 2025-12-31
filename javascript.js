d5178292c9668b82bf1094bcd86c31abbba38898
Christian Garland <239896361+christiangarland01-a11y@users.noreply.github.com>
2025-12-31 17:26:13 -0600

    Complete Etch-a-Sketch project
    For got to commit for a while. Did a bunch of stuff
    and completed the project...

--- javascript.js
+++ javascript.js
@@ -1,5 +1,10 @@
 const container = document.querySelector("#container");
+const colorBtn = document.querySelector("#color");
+const greyBtn = document.querySelector("#grey");
+
 const containerWidth = 960;
+let isColorful = true;
+let isDarker = false;
 
 function createGrid(row) {
     for (let i = 1; i <= row; i++) {
@@ -8,6 +13,7 @@ function createGrid(row) {
             div.setAttribute("id", `row${i}col${j}`);
             div.style.width = `${containerWidth / row}px`;
             div.style.height = `${containerWidth / row}px`;
+            div.style.backgroundColor = "white";
             div.classList.toggle("grid");
             container.appendChild(div);
         }
@@ -15,21 +21,48 @@ function createGrid(row) {
     
     const wholeGrid = document.querySelectorAll("#container>div");
     wholeGrid.forEach(square => {
-        square.addEventListener("mouseover", () => {
-            square.style.backgroundColor = getRandomColor();
+        square.addEventListener("mouseover", e => {
+            e.preventDefault();
+            if(isColorful) {
+                changeColor(e.target);
+            } else if(isDarker) {
+                changeTransparency(e.target);
+            }
         })
-    })
+    }) 
 }
 
-function getRandomColor() {
+function changeColor(div) {
     let hexCode = "#";
-    const hexArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
+    const hexArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
     for (let i = 1; i <= 6; i++) {
         let code = Math.floor(Math.random() * 17);
-        hexCode += `${hexArr[code]}`;
+        hexCode += `${hexArr[code]}`
     };
-    return hexCode;
+    div.style.backgroundColor = hexCode;
 }
 
-createGrid(16);
+function changeTransparency(div) {
+    if(!div.style.opacity){
+        div.style.opacity = 0;
+        div.style.backgroundColor = "black";
+    } else {
+        div.style.opacity = parseFloat(div.style.opacity) + .1;
+    }
+};
 
+window.addEventListener("load", () => createGrid(16));
+
+colorBtn.addEventListener("click", () => {
+    isColorful = true;
+    isDarker = false;
+    container.innerHTML = "";
+    createGrid(parseInt(window.prompt("How big do you want the grid", 16)));
+})
+
+greyBtn.addEventListener("click", () => {
+    isColorful = false;
+    isDarker = true;
+    container.innerHTML = "";
+    createGrid(parseInt(window.prompt("How big do you want the grid", 16)));
+})
